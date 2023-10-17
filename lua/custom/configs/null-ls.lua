e7199edcd4d2183ac766b1be94114f04bea71f2e
Aman Azad <aman@workona.com>
2023-10-17 14:10:44 -0700

    initial commit

---
+++ lua/custom/configs/null-ls.lua
@@ -0,0 +1,21 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.prettier,
+  b.diagnostics.eslint,
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
